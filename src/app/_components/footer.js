57d2173c2e3650af01b61de78b4a664ecf68bb6f
tasbiya02 <tasbiyakumail@gmail.com>
2024-10-25 00:49:54 +0530

    added link in footer

--- src/app/_components/footer.js
+++ src/app/_components/footer.js
@@ -5,15 +5,14 @@ import '../../../public/style.css'
 export default function Footer() {
   return (
     <div className="footer mt-5" >
-      <div>
-        <h5><Link href="https://shamar.co.in/" className="footer-text" style={{textDecoration:"underline"}} >Developed by shamar</Link> </h5>
-      </div>
+    <div>
+      <h5><Link href="https://tasbiya.in/" className="footer-text" style={{textDecoration:"underline"}} >Developed by tasbiya</Link> </h5>
+    </div>
 
-      <div>
-      <Link href="https://github.com/shamar0?tab=repositories" target="_blank" className="text-white footer-text" style={{margin: "0 10px",  fontSize: "2em"}}><i className="fa-brands fa-github" ></i></Link>
-      <Link href="https://www.linkedin.com/in/mohammad-shamar-867ba72ab/" target="_blank" className="text-white footer-text" style={{margin: "0 10px",  fontSize: "2em"}}><i className="fab fa-linkedin-in"></i></Link>
-      </div>
-  </div>
-    
+    <div>
+    <Link href="https://github.com/tasbiya02" target="_blank" className="text-white footer-text" style={{margin: "0 10px",  fontSize: "2em"}}><i className="fa-brands fa-github" ></i></Link>
+    <Link href="https://www.linkedin.com/in/tasbiya-21ba2a23a/" target="_blank" className="text-white footer-text" style={{margin: "0 10px",  fontSize: "2em"}}><i className="fab fa-linkedin-in"></i></Link>
+    </div>
+</div>
   )
 }
